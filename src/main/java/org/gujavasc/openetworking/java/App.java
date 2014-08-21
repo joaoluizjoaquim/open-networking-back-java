114fc85a2f794844f78db5698c381e4bb2e7d1a7
João Luiz Monteiro Joaquim <joaoluizmj@gmail.com>
2014-08-21 20:33:00 -0300

    Initial commit

---
+++ src/main/java/org/gujavasc/openetworking/java/App.java
@@ -0,0 +1,13 @@
+package org.gujavasc.openetworking.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
