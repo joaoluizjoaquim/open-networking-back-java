c4c9075d08bca79fa4112972cd60136b0ff62e0e
João Luiz Monteiro Joaquim <joaoluizmj@gmail.com>
2014-09-02 22:31:09 -0300

    Add ProjectService class, add delete and update method.

--- src/main/java/org/gujavasc/webservice/project/ProjectEndpoint.java
+++ src/main/java/org/gujavasc/webservice/project/ProjectEndpoint.java
@@ -1,33 +1,33 @@
 package org.gujavasc.webservice.project;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("projects")
-public class ProjectRestService {
+public class ProjectEndpoint {
 
-	@GET
+	@Inject
+	private ProjectService service;
+	
+	@GET()
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAll(){
-		List<Project> projects = new ArrayList<>();
-		projects.add(new Project(Long.valueOf(1), "Project 1"));
-		projects.add(new Project(Long.valueOf(2), "Project 2"));
-		return Response.ok(projects).build();
+		return Response.ok(service.getAll()).build();
 	}
 	
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response create(Project project){
-		return Response.accepted(project).build();
+		service.create(project);
+		return Response.ok().build();
 	}
 	
 	@PUT
@@ -37,9 +37,11 @@ public class ProjectRestService {
 	}
 	
 	@DELETE
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response delete(Project project){
-		return Response.accepted(project).build();
+	public Response delete(@PathParam("id") Long id){
+		service.delete(id);
+		return Response.ok().build();
 	}
 	
 }
