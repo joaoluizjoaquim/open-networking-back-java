61afb6fc6899fd120eadf2b8ac02a1831d3f2328
Joao Joaquim <joao.joaquim@supero.com.br>
2014-11-26 09:25:06 -0200

    Integration test OK.

--- src/test/java/org/gujavasc/opennetworking/test/event/EventIntegrationTest.java
+++ src/test/java/org/gujavasc/opennetworking/test/event/EventIntegrationTest.java
@@ -1,12 +1,20 @@
 package org.gujavasc.opennetworking.test.event;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
+
+import com.sun.messaging.jmq.io.Status;
 
 public class EventIntegrationTest {
 
 	@Test
 	public void sample(){
-		
+		Response response = ClientBuilder.newClient().target("http://localhost:8080/").request().get();
+		Assert.assertEquals(Status.OK, response.getStatus());
 	}
 	
 }
